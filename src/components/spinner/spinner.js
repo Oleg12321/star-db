e1ce17b473def2b29d1dccda7aaf67a4ac5473a7
Олег Данилович <olegdanilovic@MacBook-Air-Oleg.local>
2023-02-15 16:44:43 +0200

    first star-db

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-f9dm78ta2fh"><div className="ldio-kgzip4cmqrb">
+    <div></div>
+    <div></div>
+    <div><div></div></div>
+    <div><div></div></div>
+    </div></div>
+  )
+}
+
+export default Spinner
